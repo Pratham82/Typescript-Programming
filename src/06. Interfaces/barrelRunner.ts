447dc1e8823cbb606fed76447f3c64d71f5b1c18
Pratham82 <mali.prathamesh82@gmail.com>
2020-08-20 18:19:16 +0530

    Interfaces new example and barrel import

---
+++ src/06. Interfaces/barrelRunner.ts
@@ -0,0 +1,8 @@
+//* Importing multiple interfaces individually  */
+// import { bear } from "./Barrels/interfaces/bear.interface";
+// import { man } from "./Barrels/interfaces/man.interface";
+// import { pig } from "./Barrels/interfaces/pig.interface";
+
+//*We can do this in much better way by using barrel imports  */
+
+import { bear, man, pig } from "./Barrels";
